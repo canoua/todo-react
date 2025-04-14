839174d1d5619360a22abd81d08dda3b0b2ced70
akaryaka <a.karyaka.2dit@gmail.com>
2025-04-14 23:10:38 +0600

    add styled components, refactoring

--- src/todo/todo-header/TodoHeader.tsx
+++ src/todo/todo-header/TodoHeader.tsx
@@ -1,28 +1,62 @@
 import './TodoHeader.scss'
+import styled from 'styled-components';
 
 function TodoHeader() {
 
   function handleClick(): void {
-    const form: HTMLElement | null = document.getElementById('form');
+    // const form: HTMLElement | null = document.getElementById('form');
     // const input: HTMLElement | null = document.getElementById('input');
     // const stub: HTMLElement | null = document.getElementById('stub');
     // const list: HTMLElement | null = document.getElementById('list');
 
     // let tasks: any = [];
-    console.log(form);
-    
   }
+
+  const Header = styled.header `
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  `;
+
+  const Form = styled.form `
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `
+
+  const Title = styled.h1 `
+    width: 100%;
+    margin-bottom: 10px;
+    font-size: 30px;
+    line-height: 120%;
+    text-align: center;
+  `
+
+  const Input = styled.input `
+    padding: 5px;
+    margin-right: 10px;
+    border: none;
+    outline: none;
+    border-bottom: 1px solid #000;
+    font-size: 25px;
+    width: 90%; 
+  `
+  // Input.
 
   return(
     <>
-      <div className="todo__header">
-        <h1 className="title">Что хотите сделать?</h1>
-        <form action="#" className="todo__form" id="form">
-          <input type="text" placeholder="Дело" className="todo__input" id="input" autoComplete="on" />
+      <Header> 
+        <Title>Что хотите сделать?</Title>
+        <Form>
+          <Input placeholder='Дело'></Input>
+          {/* <input type="text" placeholder="Дело" className="todo__input" id="input" autoComplete="on" /> */}
           <button className="add-btn btn" id="add-btn" type="submit" onClick={handleClick}>+</button>
-        </form>
+        </Form>
         <div className="error" id="error">введите название своего дела</div>
-      </div>
+      </Header> 
     </>
   )  
 }
